c225759eb3f59adee3748df9f3bc167288d523f1
Sahinake <lumaraujo1992@hotmail.com>
2023-10-20 15:57:36 -0300

    reformulating functions for Adjacency List

--- grafo.cpp
+++ grafo.cpp
@@ -1,115 +1,170 @@
 #include <iostream>
 #include <fstream>
+#include <utility>
 #include <sstream>
 #include <string>
+#include <vector>
 
 #include "grafo.h"
 
 Grafo::Grafo() {
     this->V = 1;
     this->E = 0;
-    this->adj = NULL;
 }
 
+// Construtor para criar um grafo com um número especificado de vértices
 Grafo::Grafo(int V) {
     this->V = V;
     this->E = 0;
-    this->adj = new int*[V]; // Aloca um vetor de V posições
-
-    for(int i = 0; i < V; i++) {
-        this->adj[i] = new int[V]; // Aloca um vetor de V posições para cada posição do vetor de V posições
-
-        for(int j = 0; j < V; j++) {
-            this->adj[i][j] = 0;  // Inicializa todas as posições com 0
-        }
-    }
+    listaAdj.resize(V);
 }
 
 void Grafo::addAresta(int v, int w, int p) {
-    if (this->isVertice(v) && this->isVertice(w) && this->adj[v][w] == 0) {
-        this->adj[v][w] = p; // Adiciona apenas a aresta v -> w, pois o grafo é direcionado
+    if (this->isVertice(v) && this->isVertice(w)) {
+        //emplace_back adiciona o par (destino, peso) à lista de adjacências do vértice de origem
+        listaAdj[v].emplace_back(w, p);
         this->E++;
+    } else {
+        std::cerr << "Erro: v e/ou w não pertencem ao conjunto de vértices V(G)." << std::endl;
     }
+    
+}
+
+void Grafo::imprimeGrafo() {
+    for (int i = 0; i < V; i++) {
+        std::cout << "Vértice " << i << " está conectado a:";
+        for (const auto& E : listaAdj[i]) {
+            int w = E.first;
+            int p = E.second;
+            std::cout << " (" << w << ", peso " << p << ")";
+        }
+        std::cout << std::endl;
+    }
+}
+
+bool Grafo::isVertice(int v) {
+    return (v >= 0 && v < this->V);
 }
 
 int Grafo::countArestas() {
     return this->E;
 }
 
-bool Grafo::isVertice(int v) {
-    return (v <= (this->V)-1);
-}
+int Grafo::countVertices() {
+    return this->V;
+}    
 
-void Grafo::removeAresta(int v, int w, int p) {
-    if (this->existeAresta(v, w, p)) {
-        this->adj[v][w] = 0; // Remove a aresta
-        this->E--;
+// Remove a aresta (v, w) com peso p de um grafo direcionado e ponderado
+bool Grafo::removeAresta(int v, int w, int p) {
+    if (this->isVertice(v) && this->isVertice(w)) {
+        for (auto it = listaAdj[v].begin(); it != listaAdj[v].end(); ++it) {
+            if (it->first == w && it->second == p) {
+                listaAdj[v].erase(it);
+                this->E--;
+                return true; // Aresta encontrada e removida
+            }
+        }
+        std::cerr << "Erro: Aresta (" << v << ", " << w << ")com peso "<< p <<" não encontrada." << std::endl;
+        return false;
+    } else {
+        std::cerr << "Erro: v e/ou w não pertencem ao conjunto de vértices V(G)." << std::endl;
+        return false;
     }
 }
 
 bool Grafo::existeAresta(int v, int w, int p) {
-    return (this->isVertice(v) && this->isVertice(w) && this->adj[v][w] == p);
-}
-
-void Grafo::mudaPeso(int v, int w, int p, int np) {
-    if (this->existeAresta(v, w, p)) {
-        this->adj[v][w] = np;
-    }
-}
-
-void Grafo::imprimeGrafo() {
-
-    std::cout << "Vértices: (";
-    
-    for (int i = 0; i < this->V; i++ ) {
-        std::cout << " " << i;
-    }
-    std::cout << ")" << std::endl;
-
-    std::cout << "Arestas:" << std::endl;
-
-    for(int i = 0; i < this->V; i++) {
-        for (int j = 0; j < this->V; j++) {
-            if (this->adj[i][j] != 0) {
-                std::cout << "Aresta (" << i << ", " << j << ") com peso " << this->adj[i][j] << "." << std::endl;
-            }
+    for (const auto& E : listaAdj[v]) {
+        if (E.first == w && E.second == p) {
+            return true; // Aresta encontrada
         }
     }
+    return false; // Aresta não encontrada
+}
+
+bool Grafo::mudaPeso(int v, int w, int p, int np) {
+    if (this->isVertice(v) && this->isVertice(w)) {
+        for (auto& E : listaAdj[v]) {
+            if (E.first == w && E.second == p) {
+                E.second = np;
+                return true; // Peso da aresta modificado com sucesso
+            }
+        }
+        std::cerr << "Erro: Aresta (" << v << ", " << w << ")com peso p não encontrada." << std::endl;
+        return false;
+    } else {
+        std::cerr << "Erro: v = " << v << "e/ou w = " << w << "não pertencem ao conjunto de vértices V(G)." << std::endl;
+        return false;
+    }
+}
+
+void Grafo::removeGrafo() {
+    V = 0;
+    E = 0;
+    listaAdj.clear();
+}
+
+std::vector<int>Grafo::recuperaPeso(int v, int w) {
+    std::vector<int> pesos;
+
+    if (this->isVertice(v) && this->isVertice(w)) {
+        for (const auto& E : listaAdj[v]) {
+            if (E.first == w) {
+                pesos.push_back(E.second);
+            }
+        }
+    } else {
+        std::cerr << "Erro: v = " << v << "e/ou w = " << w << "não pertencem ao conjunto de vértices V(G)." << std::endl;
+    }
+
+    return pesos;
 }
 
 bool Grafo::isGrafoSimples() {
-    for(int i = 0; i < this->V; i++) {
-        for (int j = 0; j < this->V; j++) {
-            if(i == j) {
-                if(this->adj[i][j] != 0) return false;
+    for (int v = 0; v < this->V; v++) {
+        for (const auto& E : listaAdj[v]) {
+            int w = E.first;
+
+            // Verifica se há laços (arestas que ligam um vértice a ele mesmo)
+            if (v == w) {
+                return false;
             }
-            else {
-                if (this->adj[i][j] != 0 && this->adj[j][i] != 0) return false;
+
+            // Verifica se há múltiplas arestas direcionadas entre o mesmo par de vértices
+            for (const auto& outraAresta : this->listaAdj[w]) {
+                if (outraAresta.first == v) {
+                    return false;
+                }
             }
         }
     }
-    return true;
-}   
 
-void Grafo::imprimeMatrizAdj() {
-    std::cout << "Matriz de Adjacências\n"<< std::endl;
-    std::cout << "V | ";
-    
-    for (int i = 0; i < this->V; i++ ) {
-        std::cout << "\t" << i;
-    }
-    std::cout << std::endl;
-
-    for(int i = 0; i < this->V; i++) {
-        std::cout << "---------------------------------------------------------------"<< std::endl;
-        std::cout << i << " | ";
-        for (int j = 0; j < this->V; j++) {
-            std::cout << "\t" << this->adj[i][j];
-        }
-        std::cout << std::endl;
-    }
+    return true; // O grafo é simples
 }
 
-bool Grafo::isAdjacente(int v, int w) {
-    return (this->adj[v][w] != 0);
+/*
+Um grafo é uma árvore quando obedece as seguintes propriedades:
+    1. Deve ser um grafo conexo.
+    2. Não deve conter ciclos.
+    3. Deve ter exatamente n - 1 arestas, onde n é o número de vértices.*/
+
+bool Grafo::isArvore() {
+    // Número de vértices alcançáveis
+    int numVerticesAlcancaveis = 0;
+    std::vector<bool> visitado(this->V, false);
+
+    for (int v = 0; v < this->V; v++) {
+        if (!visitado[v]) {
+            numVerticesAlcancaveis++;
+            visitado[v] = true;
+            for (const auto& E : this->listaAdj[v]) {
+                int w = E.first;
+                if (visitado[w]) {
+                    return false; // Ciclo encontrado
+                }
+                visitado[w] = true;
+            }
+        }
+    }
+
+    return numVerticesAlcancaveis == this->V && this->E == (this->V - 1);
 }
--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <fstream>
+#include <utility>
 #include <sstream>
 #include <string>
+#include <vector>
+
 #include "grafo.h"
 
 #define FILENAME "grafo.txt"
@@ -40,16 +43,119 @@ int main(void) {
         return 1;
     }
 
-    // Testando remoção
-    g->removeAresta(1, 5, 11);
-    std::cout << "E: " << g->countArestas() << std::endl;
-    g->removeAresta(3, 9, 45);
+    g->addAresta(2, 1, 10);
 
+    g->imprimeGrafo();
+
+    // Testando remoção
+    if (g->removeAresta(1, 3, 12)) {
+        std::cout << "Aresta removida com sucesso." << std::endl;
+    } else {
+        std::cerr << "Falha na remoção da aresta." << std::endl;
+    }
+
+    if (g->removeAresta(3, 1, 10)) {
+        std::cout << "Aresta removida com sucesso." << std::endl;
+    } else {
+        std::cerr << "Falha na remoção da aresta." << std::endl;
+    }
+
+    if (g->removeAresta(6, 1, 7)) {
+        std::cout << "Aresta removida com sucesso." << std::endl;
+    } else {
+        std::cerr << "Falha na remoção da aresta." << std::endl;
+    }
+
+    if (g->removeAresta(3, 0, 10)) {
+        std::cout << "Aresta removida com sucesso." << std::endl;
+    } else {
+        std::cerr << "Falha na remoção da aresta." << std::endl;
+    }
+    
+
+    g->imprimeGrafo();
 
     // Testando mudança de peso
-    g->mudaPeso(4, 5, -15, -13); // Faz o L
+    if (g->mudaPeso(4, 5, -15, -13)) {
+        std::cout << "Peso da aresta modificado com sucesso." << std::endl;
+    } else {
+        std::cerr << "Falha na modificação do peso da aresta." << std::endl;
+    }
 
-    g->imprimeMatrizAdj();
+    g->imprimeGrafo();
 
-    return 0;
+    // Testando recupera peso
+    std::vector<int> pesos = g->recuperaPeso(6, 1);
+
+    if (!pesos.empty()) {
+        std::cout << "Pesos das arestas entre " << 6 << " e " << 1 << ": ";
+        for (int peso : pesos) {
+            std::cout << peso << " ";
+        }
+        std::cout << std::endl;
+    } else {
+        std::cerr << "Não foram encontradas arestas entre " << 6 << " e " << 1 << "." << std::endl;
+    }
+
+    // Testando grafo simples
+    if (g->isGrafoSimples()) {
+        std::cout << "O grafo é simples." << std::endl;
+    } else {
+        std::cout << "O grafo não é simples." << std::endl;
+    }
+
+    // Testando se é uma árvore
+    if (g->isArvore()) {
+        std::cout << "O grafo é uma árvore." << std::endl;
+    } else {
+        std::cout << "O grafo não é uma árvore." << std::endl;
+    }
+
+    if (g->removeAresta(4, 5, -13)) {
+        std::cout << "Aresta removida com sucesso." << std::endl;
+    } else {
+        std::cerr << "Falha na remoção da aresta." << std::endl;
+    }
+
+    if (g->removeAresta(1, 3, 45)) {
+        std::cout << "Aresta removida com sucesso." << std::endl;
+    } else {
+        std::cerr << "Falha na remoção da aresta." << std::endl;
+    }
+
+    if (g->removeAresta(3, 0, 13)) {
+        std::cout << "Aresta removida com sucesso." << std::endl;
+    } else {
+        std::cerr << "Falha na remoção da aresta." << std::endl;
+    }
+
+    if (g->removeAresta(2, 1, 10)) {
+        std::cout << "Aresta removida com sucesso." << std::endl;
+    } else {
+        std::cerr << "Falha na remoção da aresta." << std::endl;
+    }
+
+    if (g->removeAresta(5, 6, 3)) {
+        std::cout << "Aresta removida com sucesso." << std::endl;
+    } else {
+        std::cerr << "Falha na remoção da aresta." << std::endl;
+    }
+
+    g->addAresta(1, 3, 7);
+    g->addAresta(2, 6, 3);
+
+    g->imprimeGrafo();
+
+    if (g->isArvore()) {
+        std::cout << "O grafo é uma árvore." << std::endl;
+    } else {
+        std::cout << "O grafo não é uma árvore." << std::endl;
+    }
+
+    std::cout << "Arestas " << g->countArestas() << std::endl;; 
+    std::cout << "Vértices: " << g->countVertices() << std::endl;
+    
+    g->removeGrafo();
+    std::cout << "Grafo removido." << std::endl;
+
 }
